bb3a05ca52ffd50d1f0ef89da2c2ae1a5be91334
tiradovi <sunvit4@naver.com>
2025-04-16 20:06:44 +0900

    refactor: 코드 리팩토링

--- src/main/java/com/example/crawler/meal/service/MealItemService.java
+++ src/main/java/com/example/crawler/meal/service/MealItemService.java
@@ -1,11 +1,11 @@
 package com.example.crawler.meal.service;
 
+import com.example.crawler.meal.component.MenuKafkaProducer;
 import com.example.crawler.meal.entity.MealItem;
 import com.example.crawler.meal.repository.MealItemRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,36 +16,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class MealItemService {
 
   private final MenuKafkaProducer menuKafkaProducer;
-  private final MealItemCrawlerService mealItemCrawlerService;
+  private final MealTableCrawlerService mealTableCrawlerService;
   private final MealItemRepository mealItemRepository;
 
   // TODO: 리팩토링
   public List<MealItem> getMealItems() {
-    List<MealItem> menuList = mealItemCrawlerService.mealItemCrawler();
-    if (menuList.isEmpty()) {
-      throw new IllegalStateException("식단 데이터를 수집하지 못했습니다.");
-    }
+    List<MealItem> menuList = mealTableCrawlerService.mealTableCrawler();
 
-    List<String> existingIds = mealItemRepository.findAll().stream()
-        .map(MealItem::generateId)
-        .toList();
+    List<Integer> existingIds = mealItemRepository.findAllIds();
 
     List<MealItem> newItems = menuList.stream()
-        .filter(item -> !existingIds.contains(item.generateId()))
+        .filter(item -> !existingIds.contains(item.getId()))
         .toList();
 
     if (!newItems.isEmpty()) {
       mealItemRepository.saveAll(newItems);
-//      menuKafkaProducer.sendAllMealItems(newItems);
+      //menuKafkaProducer.sendAllMealItems(newItems);
     }
 
-    return mealItemRepository.findAll();
-  }
-
-
-  @Scheduled(fixedRate = 3600000)
-  public void scheduledMeal() {
-    log.info("식단 크롤링 스케줄 실행");
-    getMealItems();
+    return menuList;
   }
 }
