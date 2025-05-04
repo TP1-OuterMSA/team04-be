09ed1d44632f3e48391bd613619d30516763e344
JinEun <mattihn0130@gmail.com>
2025-05-04 18:37:25 +0900

    feat: 크롤링범위 넓힘

--- src/main/java/com/example/crawler/meal/service/MealMenuCrawlerService.java
+++ src/main/java/com/example/crawler/meal/service/MealMenuCrawlerService.java
@@ -5,7 +5,7 @@ import com.example.crawler.meal.component.crawling.MealMenuHtmlFetcher;
 import com.example.crawler.meal.component.crawling.MealMenuParser;
 import com.example.crawler.meal.entity.Menu;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,20 +18,35 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MealMenuCrawlerService {
 
-  private static final String TARGET_URL = "https://www.mju.ac.kr/mjukr/8595/subview.do";
+  private static final List<String> TARGET_URLS = List.of(
+          "https://www.mju.ac.kr/mjukr/8595/subview.do",
+          "https://www.mju.ac.kr/mjukr/8595/subview.do?enc=Zm5jdDF8QEB8JTJGZGlldCUyRm1qdWtyJTJGMTAlMkZ2aWV3LmRvJTNGbW9uZGF5JTNEMjAyNS4wNS4wNSUyNndlZWslM0RwcmUlMjY%3D",
+          "https://www.mju.ac.kr/mjukr/8595/subview.do?enc=Zm5jdDF8QEB8JTJGZGlldCUyRm1qdWtyJTJGMTAlMkZ2aWV3LmRvJTNGbW9uZGF5JTNEMjAyNS4wNC4yOCUyNndlZWslM0RwcmUlMjY%3D",
+          "https://www.mju.ac.kr/mjukr/8595/subview.do?enc=Zm5jdDF8QEB8JTJGZGlldCUyRm1qdWtyJTJGMTAlMkZ2aWV3LmRvJTNGbW9uZGF5JTNEMjAyNS4wNC4yMSUyNndlZWslM0RwcmUlMjY%3D",
+          "https://www.mju.ac.kr/mjukr/8595/subview.do?enc=Zm5jdDF8QEB8JTJGZGlldCUyRm1qdWtyJTJGMTAlMkZ2aWV3LmRvJTNGbW9uZGF5JTNEMjAyNS4wNC4xNCUyNndlZWslM0RwcmUlMjY%3D"
+  );
 
   private final MealMenuHtmlFetcher mealMenuHtmlFetcher;
   private final MealMenuExtractor mealMenuExtractor;
   private final MealMenuParser mealMenuParser;
 
   public List<Menu> mealMenuCrawler() {
-    try {
-      Document document = mealMenuHtmlFetcher.fetch(TARGET_URL);
-      Element table = mealMenuExtractor.extractWeeklyMenu(document);
-      return mealMenuParser.parse(table);
-    } catch (IOException e) {
-      log.error("식단 테이블 크롤링 중 오류 발생", e);
-      return Collections.emptyList();
+    List<Menu> totalMenuList = new ArrayList<>();
+
+    for (String url : TARGET_URLS) {
+      try {
+        log.info("식단 크롤링 시작 - URL: {}", url);
+        Document document = mealMenuHtmlFetcher.fetch(url);
+        Element table = mealMenuExtractor.extractWeeklyMenu(document);
+        List<Menu> weeklyMenus = mealMenuParser.parse(table);
+        totalMenuList.addAll(weeklyMenus);
+        log.info("식단 크롤링 성공 - {}건 수집됨", weeklyMenus.size());
+      } catch (IOException e) {
+        log.error("식단 크롤링 실패 - URL: {}", url, e);
+      }
     }
+
+    log.info("전체 식단 수집 완료 - 총 {}건", totalMenuList.size());
+    return totalMenuList;
   }
 }
